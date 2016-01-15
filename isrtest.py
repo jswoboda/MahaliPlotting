2e9497b31f84af287c0315d5717f8df46f73ae2b
John Swoboda <swoboj@bu.edu>
2016-01-15 13:52:41 -0500

    added isrtest.py

---
+++ isrtest.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""
+Created on Fri Jan 15 12:48:45 2016
+
+@author: John Swoboda
+"""
+
+import os
+import numpy as np
+from plotdata import getSRIhdf5
+
+
+
+if __name__== '__main__':
+    isrdir= os.path.expanduser('~/DATA/Mahali/ISRdata/2015_10_11')
+    filename= os.path.join(isrdir,'20151011.004_lp_3min-cal.h5')
+    xvec = np.linspace(-150.,150.,256)
+    yvec = np.linspace(-50.,150.,256)
+    Xmat,Ymat=np.meshgrid(xvec,yvec)
+    xycoords = np.column_stack((Xmat.flatten(),Ymat.flatten()))
+    newcordname='Cartesian'
+    vbounds=[[1e10,5e11],[1500.,3000.],[1500.,3000.]]
+    times = ['2015-10-11 10:00:00','2015-10-11 12:00:00']
+    pnheights=[('Ne',150.),('Ti',250.),('Te',250.)]
+    getSRIhdf5(filename,times,pnheights,xycoords,newcordname,vbounds)
