2bd8563286ad6c92eb46b5fcea584ae24b3e269a
John Swoboda <swoboj@bu.edu>
2016-03-03 12:06:20 -0500

    updates to bash scripts

--- examplewbounds.sh
+++ examplewbounds.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python plotdata.py -a ~/DATA/Mahali/Allsky558/2015_10_07/ -w 558 -i ~/DATA/Mahali/GPS_Processed_IonoFiles/site_280_2015/ -t .05 -p plots10072015JSALL/ -d 10/07/2015 -b 6:04 -e 6:17 -r y -l 45 -l 75 -o -175 -o -125
+python plotdata.py -a ~/DATA/Mahali/Allsky558/2015_10_07/ -w 558 -i ~/DATA/Mahali/GPS_Processed_IonoFiles/site_280_2015/ -t .2 -p plots10072015_proc03032016/ -d 10/07/2015 -b 6:00 -e 6:30 -r y -l 55 -l 75 -o -170 -o -130
